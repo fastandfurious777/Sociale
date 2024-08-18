86f64f70e61f45a882c6954a153202deaded725b
Antoni Pokrzywa <antonipokrzywa@gmail.com>
2024-08-18 18:54:00 +0200

    Added HTML landing page with User authentication functionality

---
+++ bike_map/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Bike(models.Model):
+    name = models.CharField()
+    #lat and longtitude, maby with geodjango
+    encrypted_code = models.TextField()
+    is_available = models.BooleanField()
+
+    
