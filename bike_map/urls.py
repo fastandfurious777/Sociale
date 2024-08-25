5a5828d69f7a07832d260d5888ea89cd0132cf85
Antoni Pokrzywa <antonipokrzywa@gmail.com>
2024-08-25 22:17:43 +0200

    Add API endpoints for bike management

--- bike_map/urls.py
+++ bike_map/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="map-home"),
-     path('bike-positions/', views.bike_positions, name="bike-positions"),
+    path('scanner', views.scanner, name="scanner"),
+    path('get-bike-positions/', views.get_bike_positions, name="get-bike-positions"),
+    path('get-bike-code/', views.get_bike_code, name='get-bike-code'),
+    path('start-rental/', views.start_rental, name='start-rental'),
+    path('end-rental/', views.end_rental, name='end-rental'),
 
 ]
