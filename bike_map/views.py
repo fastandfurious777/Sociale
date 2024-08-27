0c37bab8e95d2ab17121565638dd67f7f8227d59
Antoni Pokrzywa <antonipokrzywa@gmail.com>
2024-08-27 14:39:25 +0200

    add basic UI and security enhancements

--- bike_map/views.py
+++ bike_map/views.py
@@ -4,11 +4,15 @@ from .models import Bike, Rental
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from cryptography.fernet import Fernet
+from django.conf import settings
 
 @login_required
 def home(request):
-    bikes = Bike.objects.filter(is_available=True).values()
-    return render(request,"bike_map/index.html",context = {"bikes": list(bikes)})
+    user = request.user
+    return render(request,"bike_map/map-index.html",context={"user": user})
+
+
 @login_required
 def scanner(request):
     return render(request,"bike_map/scanner.html")
@@ -25,15 +29,18 @@ def get_bike_positions(request):
 def get_bike_code(request):
     if request.method == "GET":
         try:
-            bike_id = int(request.GET.get("qr_code"))
+            bike_id = int(request.GET.get("bike_id"))
             bike = Bike.objects.get(id=bike_id)
+            fernet = Fernet(settings.CIPHER_KEY)
+            bike_code = bike.code 
+            '''fernet.decrypt(bike.code).decode()'''
         except ValueError:
             return JsonResponse({"message": "Wrong QR code provided"}, status = 400)
         except Exception:
             return JsonResponse({"message": "Some unexpected error ocured"}, status= 500)
         finally:
             return JsonResponse(
-                {"message": "Code successfully scanned", "bike_code": bike.encrypted_code}, status=200
+                {"message": "Code successfully scanned", "bike_code": bike_code}, status=200
 
             )
 
@@ -43,13 +50,15 @@ def start_rental(request):
         try:
             user = request.user
             bike_id = json.loads(request.body.decode("utf-8"))
-            #If User has no rentals at that time
+            bike = Bike.objects.get(id=bike_id)
             if not Rental.objects.filter(is_completed=False).filter(user=user):
-                Rental(user=user,bike=Bike.objects.get(id=bike_id))
-                pass
+                Rental(user=user,bike=bike)
+                return JsonResponse(
+                    {"message" : "Ride successfully started"}, status=200
+                )
         except Exception as e:
             return JsonResponse(
-                {"message" : f"Unable to end a ride, {e}"}, status=500
+                {"message" : f"Unable to start a ride, {e}"}, status=500
             )
 
 
@@ -67,4 +76,18 @@ def end_rental(request):
             return JsonResponse(
                 {"message" : f"You successfully finished your ride"}, status=200
             )
-
+"""      
+def get_user_status(request):
+    user = request.user
+    #If there is incompleted rental by user
+    active_rental = Rental.objects.filter(is_completed=False).filter(user=user)
+    if active_rental:
+        context = {
+            "rental": active_rental,
+            #TODO: Check if needed
+            "bike": active_rental.bike
+        }
+        return render(request,"bike_map/map-index.html",context)
+    else:
+        return render(request,"bike_map/map-index.html")
+ """
