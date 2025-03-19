da901ce896dd91fc1a3c269814300fdb78793c65
Antoni Pokrzywa <68478419+AntoniPokrzywa@users.noreply.github.com>
2025-03-19 19:08:59 +0000

    fixed views and added logger

--- users/api/views.py
+++ users/api/views.py
@@ -3,15 +3,14 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
-from rest_framework import status, permissions, authentication
+from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.views import APIView
 
 from users.selectors import user_list, user_get, user_get_by_email
 from users.services import user_create, user_update, user_delete
-from utils.emails import send_verification_email, send_password_reset_email
-from .serializers import (
+from users.api.serializers import (
     UserSerializer,
     LoginSerializer,
     RegisterSerializer,
@@ -20,15 +19,15 @@ from .serializers import (
     ResetPasswordSerializer,
     VerifyEmailSerializer,
 )
+from utils.emails import send_verification_email, send_password_reset_email
+from utils.permissions import AdminPermissionMixin
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class LoginThrottle(UserRateThrottle):
-    rate = '5/min'
-
-class AdminPermissionMixin:
-    """Mixin to enforce admin  only access and session auth"""
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAdminUser]
+    rate = "5/min"
 
 
 class UserLoginApi(APIView):
@@ -39,20 +38,29 @@ class UserLoginApi(APIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
-        user = user_get_by_email(email=data['email'])
-        if user is None or not user.check_password(data['password']) or not user.is_eligible:
-            return Response(
-            {"detail": "Invalid credentials"},
-            status=status.HTTP_400_BAD_REQUEST
+
+        user = user_get_by_email(email=data["email"])
+        if not user.check_password(data["password"]):
+            logger.warning(
+                "INVALID CREDENTIALS: %s", user.email, extra={"request": request}
             )
-       
+            return Response(
+                {"detail": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not user.is_eligible:
+            logger.warning(
+                "ACCOUNT NOT ACTIVE: %s", user.email, extra={"request": request}
+            )
+            return Response(
+                {"detail": "Your account needs to be activated by an admin"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         login(request, user)
-        return Response(
-            {"detail": "Successfully logged in"},
-            status=status.HTTP_200_OK
-        )
-        
+        logger.info("SUCCESSFUL LOGIN: %s", user.email, extra={"request": request})
+        return Response({"detail": "Successfully logged in"}, status=status.HTTP_200_OK)
+
 
 class UserRegisterApi(APIView):
     permission_classes = [permissions.AllowAny]
@@ -68,11 +76,18 @@ class UserRegisterApi(APIView):
         try:
             send_verification_email(user.email, uid, token)
         except Exception as e:
-            #TODO add logger
-            return Response({"detail": "Something unexpected happened."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        return Response({"detail": "Account created. Please verify your email."}, status=status.HTTP_201_CREATED)
-        
+            logger.error("MAIL SENDING FAILED: %s", e, extra={"request": request})
+            return Response(
+                {"detail": "Mail sending failed"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        logger.info("CREATED ACCOUNT: %s", user.email, extra={"request": request})
+        return Response(
+            {"detail": "Account created. Please verify your email."},
+            status=status.HTTP_201_CREATED,
+        )
+
 
 class UserListApi(AdminPermissionMixin, APIView):
     serializer_class = UserSerializer
@@ -88,13 +103,9 @@ class UserDetailApi(AdminPermissionMixin, APIView):
 
     def get(self, request, user_id):
         user = user_get(user_id=user_id)
-        if user is None:
-            return Response(
-                {"detail": "User not found"},
-                status=status.HTTP_404_NOT_FOUND
-            )
         serializer = self.serializer_class(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class UserCreateApi(AdminPermissionMixin, APIView):
     serializer_class = RegisterSerializer
@@ -104,31 +115,32 @@ class UserCreateApi(AdminPermissionMixin, APIView):
         serializer.is_valid(raise_exception=True)
         user_create(**serializer.validated_data)
 
-        return Response({"detail": "Account created."}, status=status.HTTP_201_CREATED)
-    
+        logger.info(
+            "CREATED USER: %s",
+            serializer.validated_data["email"],
+            extra={"request": request},
+        )
+        return Response(status=status.HTTP_201_CREATED)
+
+
 class UserUpdateApi(AdminPermissionMixin, APIView):
     serializer_class = UpdateSerializer
 
     def put(self, request, user_id: int):
         serializer = self.serializer_class(data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
-        user = user_update(user_id=user_id, data=serializer.validated_data)
-        if user is not None:
-            return Response({"detail": "Updated successfully"}, status=status.HTTP_200_OK)
-        return Response(
-            {"detail": "Update failed"},
-            status=status.HTTP_400_BAD_REQUEST
-        )
+
+        user_update(user_id=user_id, data=serializer.validated_data)
+
+        logger.info("UPDATED USER: %d", user_id, extra={"request": request})
+        return Response(status=status.HTTP_200_OK)
 
 
 class UserDeleteApi(AdminPermissionMixin, APIView):
     def delete(self, request, user_id: int):
-        if not user_get(user_id=user_id):
-            return Response(
-                {"detail": "User not found"},
-                status=status.HTTP_404_NOT_FOUND
-            )
         user_delete(user_id=user_id)
+
+        logger.info("DELETED USER: %d", user_id, extra={"request": request})
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -139,20 +151,30 @@ class VerifyEmailApi(APIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
-        try:
-            user_id = int(force_str(urlsafe_base64_decode(data['uid'])))
-            user = user_get(user_id=user_id)
-            if user is None:
-                return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND) 
-        except (TypeError, ValueError, OverflowError):
-            return Response({"detail": "Invalid verification data"}, status=status.HTTP_400_BAD_REQUEST)
 
-        if default_token_generator.check_token(user, data['token']):
-            user_update(user_id=user_id, data={'is_active': True})
-            return Response({"detail": "Account verified successfully"},status=status.HTTP_200_OK)
-        else:
+        try:
+            user_id = int(force_str(urlsafe_base64_decode(data["uid"])))
+        except (TypeError, ValueError, OverflowError):
+            logger.error("UID DECODING FAILED: invalid uid", extra={"request": request})
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+        except Exception as e:
+            logger.error("UID DECODING FAILED: %s", e, extra={"request": request})
+            return Response(
+                {"detail": "Something unexpected happened"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        user = user_get(user_id=user_id)
+        if default_token_generator.check_token(user, data["token"]):
+            user.activate()
+            logger.info("ACCOUNT VERIFIED: %s", user.email, extra={"request": request})
+            return Response(
+                {"detail": "Account verified successfully"}, status=status.HTTP_200_OK
+            )
+
+        logger.error("TOKEN CHECK FAILED: invalid token", extra={"request": request})
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 class ResetPasswordRequestApi(APIView):
 
@@ -163,19 +185,26 @@ class ResetPasswordRequestApi(APIView):
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
 
-        user = user_get_by_email(data['email'])
-        if user is None:
-            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-        
+        user = user_get_by_email(data["email"])
+
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
         try:
             send_password_reset_email(user.email, uid, token)
-        except Exception:
-            # TODO Logger and specify exceptions that can happen
-            return Response({"detail": "Something unexpected happened."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        return Response({"detail": "Your password reset link has been sent"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error("MAIL SENDING FAILED: %s", e, extra={"request": request})
+            return Response(
+                {"detail": "Mail sending failed"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        logger.info(
+            "PASSWORD RESET REQUEST: %s", user.email, extra={"request": request}
+        )
+        return Response(
+            {"detail": "Your password reset link has been sent"},
+            status=status.HTTP_200_OK,
+        )
 
 
 class ResetPasswordCheckApi(APIView):
@@ -187,16 +216,28 @@ class ResetPasswordCheckApi(APIView):
         serializer.is_valid(raise_exception=True)
         data = serializer.validated_data
 
-        user_id = int(force_str(urlsafe_base64_decode(data['uid'])))
+        try:
+            user_id = int(force_str(urlsafe_base64_decode(data["uid"])))
+        except (TypeError, ValueError, OverflowError):
+            logger.error("UID DECODING FAILED: invalid uid", extra={"request": request})
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logger.error("UID DECODING FAILED: %s", e, extra={"request": request})
+            return Response(
+                {"detail": "Something unexpected happened"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
         user = user_get(user_id=user_id)
 
-        if user is None:
-            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-    
-        if default_token_generator.check_token(user, data['token']):
-            user.set_password(data['password'])
+        if default_token_generator.check_token(user, data["token"]):
+            user.set_password(data["password"])
             user.full_clean()
             user.save()
-            return Response({"detail": "Your password has been changed successfully"},status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "Your password has been changed successfully"},
+                status=status.HTTP_200_OK,
+            )
+
+        logger.error("TOKEN CHECK FAILED: invalid token", extra={"request": request})
+        return Response(status=status.HTTP_400_BAD_REQUEST)
